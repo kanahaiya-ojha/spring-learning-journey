05937dc1d79a0bdc0faa1303703c8eefda2e4f45
kanahaiya-ojha <kanahaiyaojha@gmail.com>
2026-01-07 19:01:44 +0530

    Implement autowiring

---
+++ spring-core-practice/src/main/java/com/kanahaiya/practice/annotations/AnnotationsPractice.java
@@ -0,0 +1,13 @@
+package com.kanahaiya.practice.annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationsPractice {
+   public static void main(String[] args) {
+	
+	   ApplicationContext context=new ClassPathXmlApplicationContext("com/kanahaiya/practice/annotations/applicationContext.xml");
+	   MandiPriceService mandiPriceService=(MandiPriceService) context.getBean("mandiPriceService");
+	   mandiPriceService.processPrices();
+}
+}
