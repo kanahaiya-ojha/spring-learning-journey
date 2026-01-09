10f5dca85bebf7389726e396750f44e201ddc890
kanahaiya-ojha <kanahaiyaojha@gmail.com>
2026-01-09 17:36:38 +0530

    feat: using path variable

--- mandi-app/mandi-app/src/main/java/com/kanahaiya/mandi/MandiController.java
+++ mandi-app/mandi-app/src/main/java/com/kanahaiya/mandi/MandiController.java
@@ -1,6 +1,7 @@
 package com.kanahaiya.mandi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,13 @@ public class MandiController {
     public String greetUser(@RequestParam(name="user", defaultValue="Guest") String userName) {
     	return "Namaste " + userName + "!" + priceService.getFormattedPrice();
     }
+    
+ // Use {city} to tell Spring this part of the URL is a variable
+    @GetMapping("/info/{city}")
+    public String getCityInfo(@PathVariable("city") String cityName) {
+        // This picks up the value from the URL path
+        return "Showing Mandi Report for: " + cityName.toUpperCase() + 
+               " | " + priceService.getFormattedPrice();
+    }
+    
 }
