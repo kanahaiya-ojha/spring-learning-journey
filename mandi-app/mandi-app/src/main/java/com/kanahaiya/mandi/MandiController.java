0d98c9c73ae962a38ee73628d45a0d4c32573129
kanahaiya-ojha <kanahaiyaojha@gmail.com>
2026-01-09 17:21:36 +0530

    feat: Passing Data from URL to Java using @RequestParam

--- mandi-app/mandi-app/src/main/java/com/kanahaiya/mandi/MandiController.java
+++ mandi-app/mandi-app/src/main/java/com/kanahaiya/mandi/MandiController.java
@@ -1,6 +1,7 @@
 package com.kanahaiya.mandi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class MandiController {
     @GetMapping("/prices") // Visit http://localhost:8081/prices
     public String showPrices() {
         return priceService.getFormattedPrice();
+    }
+    
+    @GetMapping("/greet")
+    public String greetUser(@RequestParam(name="user", defaultValue="Guest") String userName) {
+    	return "Namaste " + userName + "!" + priceService.getFormattedPrice();
     }
 }
